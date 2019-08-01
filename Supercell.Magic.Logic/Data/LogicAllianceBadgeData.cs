06ff9cb9baa27322977e90c9bf6983c6ffa70438
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2019-08-01 23:16:53 +0300

    Update LogicAllianceBadgeData.cs

--- Supercell.Magic.Logic/Data/LogicAllianceBadgeData.cs
+++ Supercell.Magic.Logic/Data/LogicAllianceBadgeData.cs
@@ -4,9 +4,9 @@ namespace Supercell.Magic.Logic.Data
 
     public class LogicAllianceBadgeData : LogicData
     {
-        private string m_iconLayer0;
-        private string m_iconLayer1;
-        private string m_iconLayer2;
+        private LogicAllianceBadgeLayerData m_iconLayer0;
+        private LogicAllianceBadgeLayerData m_iconLayer1;
+        private LogicAllianceBadgeLayerData m_iconLayer2;
 
         public LogicAllianceBadgeData(CSVRow row, LogicDataTable table) : base(row, table)
         {
@@ -17,9 +17,24 @@ namespace Supercell.Magic.Logic.Data
         {
             base.CreateReferences();
 
-            this.m_iconLayer0 = this.GetValue("IconLayer0", 0);
-            this.m_iconLayer1 = this.GetValue("IconLayer1", 0);
-            this.m_iconLayer2 = this.GetValue("IconLayer2", 0);
+            this.m_iconLayer0 = LogicDataTables.GetAllianceBadgeLayerByName(this.GetValue("IconLayer0", 0), this);
+            this.m_iconLayer1 = LogicDataTables.GetAllianceBadgeLayerByName(this.GetValue("IconLayer1", 0), this);
+            this.m_iconLayer2 = LogicDataTables.GetAllianceBadgeLayerByName(this.GetValue("IconLayer2", 0), this);
+        }
+
+        public LogicAllianceBadgeLayerData GetIconLayer0()
+        {
+            return this.m_iconLayer0;
+        }
+
+        public LogicAllianceBadgeLayerData GetIconLayer1()
+        {
+            return this.m_iconLayer1;
+        }
+
+        public LogicAllianceBadgeLayerData GetIconLayer2()
+        {
+            return this.m_iconLayer2;
         }
     }
 }
