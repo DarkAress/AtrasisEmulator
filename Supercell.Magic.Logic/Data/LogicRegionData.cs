5a3c88d87cfb26ce9e55b97518c1268e5db2bc8e
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-02-01 12:57:00 +0300

    Update LogicRegionData.cs

--- Supercell.Magic.Logic/Data/LogicRegionData.cs
+++ Supercell.Magic.Logic/Data/LogicRegionData.cs
@@ -4,6 +4,11 @@ namespace Supercell.Magic.Logic.Data
 
     public class LogicRegionData : LogicData
     {
+        private string m_displayName;
+
+        private bool m_isCountry;
+        private bool m_hS;
+
         public LogicRegionData(CSVRow row, LogicDataTable table) : base(row, table)
         {
             // LogicRegionData.
@@ -12,6 +17,25 @@ namespace Supercell.Magic.Logic.Data
         public override void CreateReferences()
         {
             base.CreateReferences();
+
+            this.this.m_displayName = this.GetValue("DisplayName", 0);
+            this.m_isCountry = this.GetBooleanValue("IsCountry", 0);
+            this.m_hS = this.GetBooleanValue("HS", 0);
+        }
+
+        public String GetDisplayName()
+        {
+          return this.m_displayName;
+        }
+
+        public bool IIsCountry()
+        {
+          return this.m_isCountry;
+        }
+
+        public bool IsHS()
+        {
+          return this.m_hS;
         }
     }
 }
